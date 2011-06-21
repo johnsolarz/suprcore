4bdfa8578b62b975c29f8d03ff2e04aa0ff98fb2
John Solarz <johnsolarz@gmail.com>
2011-06-21 12:03:03 -0500

    renamed custom-post.php for continuity

--- functions.php
+++ functions.php
@@ -450,4 +450,4 @@ function your_twitter_icon() {
  * Include primary project functions in separate folder.  
  */
 
-//include('functions/custom-posts.php');
+//include('functions/custom-post.php');
