cc4cd11775ad100286a9766f1ec8c77adc33e045
John Solarz <johnsolarz@gmail.com>
2011-06-27 23:15:54 -0500

    modified grid

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,6 +1,6 @@
 <?php
 
-	if (   ! is_active_sidebar( 'footer-widget-1' )
+	if ( ! is_active_sidebar( 'footer-widget-1' )
 		&& ! is_active_sidebar( 'footer-widget-2' )
 		&& ! is_active_sidebar( 'footer-widget-3' )
 		&& ! is_active_sidebar( 'footer-widget-4' )
@@ -12,25 +12,25 @@
 <aside class="grid_12 alpha omega" role="complementary">
 
 <?php if ( is_active_sidebar( 'footer-widget-1' ) ) : ?>
-	<ul class="grid_2 alpha">
+	<ul class="grid_3 alpha">
 		<?php dynamic_sidebar( 'footer-widget-1' ); ?>
 	</ul>
 <?php endif; ?>
 
 <?php if ( is_active_sidebar( 'footer-widget-2' ) ) : ?>
-	<ul class="grid_2">
+	<ul class="grid_3">
 		<?php dynamic_sidebar( 'footer-widget-2' ); ?>
 	</ul>
 <?php endif; ?>
 
 <?php if ( is_active_sidebar( 'footer-widget-3' ) ) : ?>
-	<ul class="grid_2">
+	<ul class="grid_3">
 		<?php dynamic_sidebar( 'footer-widget-3' ); ?>
 	</ul>
 <?php endif; ?>
 
 <?php if ( is_active_sidebar( 'footer-widget-4' ) ) : ?>
-	<ul class="grid_2">
+	<ul class="grid_3 omega">
 		<?php dynamic_sidebar( 'footer-widget-4' ); ?>
 	</ul>
 <?php endif; ?>
