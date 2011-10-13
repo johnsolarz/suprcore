516191409baead0cf27a20d1686ae2e47e423ede
John Solarz <johnsolarz@gmail.com>
2011-10-12 21:25:03 -0500

    whitespace

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,28 +1,19 @@
-
-
 <aside class="clearfix" role="complementary">
 
-
 	<div class="grid_3 alpha">
-		<?php dynamic_sidebar( 'Footer 1' ); ?>
+		<?php dynamic_sidebar('Footer 1'); ?>
 	</div>
-
-
 
 	<div class="grid_3">
-		<?php dynamic_sidebar( 'Footer 2' ); ?>
+		<?php dynamic_sidebar('Footer 2'); ?>
 	</div>
-
-
 
 	<div class="grid_3">
-		<?php dynamic_sidebar( 'Footer 3' ); ?>
+		<?php dynamic_sidebar('Footer 3'); ?>
 	</div>
-
 
 	<div class="grid_3 omega">
-		<?php dynamic_sidebar( 'Footer 4' ); ?>
+		<?php dynamic_sidebar('Footer 4'); ?>
 	</div>
-
 
 </aside>
