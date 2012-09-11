3ec2e5a59135f63fc645cc659c78fd68ba7c6eba
John Solarz <johnsolarz@gmail.com>
2012-09-11 15:49:36 -0500

    update to Modernizr v2.6.2 and jQuery v1.8.1

--- header.php
+++ header.php
@@ -18,9 +18,9 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/grid.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/suprcore.css">
 
-    <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-2.6.1.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 
